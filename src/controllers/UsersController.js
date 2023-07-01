4c39c61e034230812455ffec10b46a2dfa9be569
Eduardo Berneira <duduberneira.drb@gmail.com>
2023-07-01 14:52:37 -0300

    Initial commit

---
+++ src/controllers/UsersController.js
@@ -0,0 +1,95 @@
+const { hash, compare } = require("bcryptjs");
+const AppError = require("../utils/AppError")
+const sqliteConnection = require('../database/sqlite')
+
+class UsersController {
+  async create(request, response) {
+    const { name, email, password } = request.body
+    
+    const database = await sqliteConnection()
+    
+    const checkUserExist = await database.get("SELECT * FROM users WHERE email = (?)", [email])
+
+    if(checkUserExist) {
+      throw new AppError("Este email já está cadastrado.")
+    }
+
+    const hashedPassword = await hash(password, 8);
+    console.log(hashedPassword)
+
+    await database.run(
+      "INSERT INTO users (name, email, password) VALUES (?, ?, ?)", 
+      [name, email, hashedPassword])
+
+    return response.status(201).json()
+  }
+
+  async update(request, response) {
+    const { name, email, password, old_password} = request.body
+    const user_id = request.user.id
+
+    const database = await sqliteConnection()
+
+    const user = await database.get("SELECT * FROM users WHERE id = (?)", [user_id])
+
+    if(!user) {
+      throw new AppError("Usuário não encontrado")
+    }
+
+    const userWithUpdatedEmail = await database.get("SELECT * FROM users WHERE email = (?)", [email])
+
+    if(userWithUpdatedEmail && userWithUpdatedEmail.id !== user.id) {
+      throw new AppError('Este email já está em uso.')
+    }
+
+    user.name = name ?? user.name
+    user.email = email ?? user.email
+
+    if(password && !old_password){
+      throw new AppError('Você precisa informar a senha antiga')
+    }
+
+    if(password && old_password){
+      const checkOldPassword = await compare(old_password, user.password)
+
+      if(!checkOldPassword){
+        throw new AppError('As senhas nao conferem')
+      }
+
+      user.password = await hash(password, 8)
+    }
+
+    await database.run(`
+      UPDATE users SET
+      name = ?,
+      email = ?,
+      password = ?,
+      updated_at = DATETIME('now')
+      WHERE id = ?`, 
+      [user.name, user.email, user.password, user_id] 
+    )
+
+    return response.json()
+
+  }
+
+  async show(request, response) {
+    const database = await sqliteConnection()
+
+    const users = await database.get("SELECT * FROM users")
+
+    return response.json(users)
+  }
+
+  async delete(request, response) {
+    const { id } = request.params
+
+    const database = await sqliteConnection()
+
+    await database.get("DELETE FROM users WHERE id = ?", [id])
+
+    return response.json()
+  }
+}
+
+module.exports = UsersController  
